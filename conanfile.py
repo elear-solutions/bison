5811d6cecc73883d6abd4305d5e4ef39ec5a051b
Prasad Makireddy <prasadmakireddy@elear.solutions>
2021-07-28 13:02:17 +0530

    Updates conanfile

--- conanfile.py
+++ conanfile.py
@@ -31,32 +31,13 @@ class BisonLibConan(ConanFile):
         extracted_dir = "bison-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
-    def build_requirements(self):
-        if self.settings.compiler == "Visual Studio":
-            self.build_requires("automake/1.16.2")
-
     def configure(self):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
     @contextmanager
     def _build_context(self):
-        if self.settings.compiler == "Visual Studio":
-            with tools.vcvars(self.settings):
-                build_env = {
-                    "CC": "{} cl -nologo".format(tools.unix_path(self.deps_user_info["automake"].compile)),
-                    "CXX": "{} cl -nologo".format(tools.unix_path(self.deps_user_info["automake"].compile)),
-                    "CFLAGS": "-{}".format(self.settings.compiler.runtime),
-                    "LD": "link",
-                    "NM": "dumpbin -symbols",
-                    "STRIP": ":",
-                    "AR": "{} lib".format(tools.unix_path(self.deps_user_info["automake"].ar_lib)),
-                    "RANLIB": ":",
-                }
-                with tools.environment_append(build_env):
-                    yield
-        else:
-            yield
+        yield
 
     def _configure_autotools(self):
         if self._autotools:
@@ -68,15 +49,6 @@ class BisonLibConan(ConanFile):
             "--datarootdir={}".format(os.path.join(self.package_folder, "bin", "share").replace("\\", "/")),
         ]
         host, build = None, None
-        if self.settings.compiler == "Visual Studio":
-            # Avoid a `Assertion Failed Dialog Box` during configure with build_type=Debug
-            # Visual Studio does not support the %n format flag:
-            # https://docs.microsoft.com/en-us/cpp/c-runtime-library/format-specification-syntax-printf-and-wprintf-functions
-            # Because the %n format is inherently insecure, it is disabled by default. If %n is encountered in a format string,
-            # the invalid parameter handler is invoked, as described in Parameter Validation. To enable %n support, see _set_printf_count_output.
-            args.extend(["gl_cv_func_printf_directive_n=no", "gl_cv_func_snprintf_directive_n=no", "gl_cv_func_snprintf_directive_n=no"])
-            self._autotools.flags.append("-FS")
-            host, build = False, False
         self._autotools.configure(args=args, configure_dir= "..", host=host, build=build)
         return self._autotools
 
@@ -90,10 +62,6 @@ class BisonLibConan(ConanFile):
             env_build = self._configure_autotools()
             env_build.install()
             self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-
-        if self.settings.compiler == "Visual Studio":
-            os.rename(os.path.join(self.package_folder, "lib", "liby.a"),
-                      os.path.join(self.package_folder, "lib", "y.lib"))
 
     def package_info(self):
         self.cpp_info.libs = ["y"]
